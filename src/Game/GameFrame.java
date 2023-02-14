59bdf264fb6488d28ecbee731af1aeaf554b668c
fhefkkk <hee.fan@outlook.com>
2023-02-14 15:00:10 +0800

    添加角色显示

--- src/Game/GameFrame.java
+++ src/Game/GameFrame.java
@@ -8,44 +8,66 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 public class GameFrame extends JFrame {
-    // 双缓冲机制-定义双缓存图片
-    Image offScreenImage = null;
-
-    int width = 1600;
-    int height = 900;
 
     Player player = new Player("src\\Image\\playerdown.png", 100, 200, this, "src\\Image\\playerup.png",
             "src\\Image\\playerdown.png", "src\\Image\\playerleft.png", "src\\Image\\playerright.png");
 
+    UpdateThread ut;
+
     public GameFrame() {
         setSize(1600, 900); // 设置窗口大小
         setLocationRelativeTo(null); // 把窗口位置设置到屏幕中心
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);// 关闭窗口模式
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// 关闭窗口模式
         setResizable(false); // 窗口固定
 
         JPanel gamePan = new JPanel();
+        gamePan.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(gamePan);
+        gamePan.setLayout(null);
+
+        JPanel panel = new MyPanel();
+        panel.setBounds(0, 0, 1600, 900);
+        gamePan.add(panel);
+        panel.setLayout(null);
 
         setVisible(true);
+
+        ut = new UpdateThread(panel);
+        ut.start();
+    }
+
+    class MyPanel extends JPanel {
+        public void paint(Graphics g) {
+            // super.paint(g);
+            ImageIcon map1 = new ImageIcon("src\\Image\\map1.png");
+
+            g.drawImage(map1.getImage(), 0, 0, null);
+
+            player.paintSelf(g);
+        }
+    }
+}
+
+class UpdateThread extends Thread {
+    JPanel panel;
+
+    public UpdateThread(JPanel panel) {
+        this.panel = panel;
     }
 
     @Override
-    public void paint(Graphics g) {
-        // 双缓存-创建和容器大小相同的Image图片
-        if (offScreenImage == null) {
-            offScreenImage = this.createImage(width, height);
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            panel.repaint();
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        // 获取图片画笔
-        Graphics gImage = offScreenImage.getGraphics();
-
-        // 内容
-        Image bg = Toolkit.getDefaultToolkit().getImage("src\\Image\\map1.png");
-        repaint();
-        player.paintSelf(gImage);
-        g.drawImage(bg, 0, 0, this);
-        // 将缓存区绘制好的图形整个绘制到容器的画布中
-        g.drawImage(offScreenImage, 0, 0, null);
-
     }
+
 }
